8674a2861c25c7c152e03a1a71980098e08b2c2b
Alexander Hodes <alexander.hodes@live.com>
2020-01-25 15:22:15 +0100

    added input to search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import {PlatformService} from "~/app/shared/platform.service";
+import {GeolocationService} from "~/app/shared/geolocation.service";
+import {TextField} from "tns-core-modules/ui/text-field";
+import {inputType} from "tns-core-modules/ui/dialogs";
+import text = inputType.text;
 
 @Component({
     selector: "Search",
@@ -7,12 +11,26 @@ import {PlatformService} from "~/app/shared/platform.service";
 })
 export class SearchComponent implements OnInit {
 
-    constructor(private platformService: PlatformService) {
+    name: string;
+
+    constructor(private platformService: PlatformService,
+                private geoLocationService: GeolocationService) {
         // Use the constructor to inject services.
         this.platformService.getProperties();
     }
 
     ngOnInit(): void {
         // Use the "ngOnInit" handler to initialize data for the view.
+    }
+
+    trackPosition(): void {
+        this.geoLocationService.getCurrentLocation();
+    }
+
+    onReturnPress(event: any): void {
+        let textField = <TextField> event.object;
+
+        console.log('name', textField.text);
+        console.log('ngModel', textField.text);
     }
 }
--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -3,11 +3,13 @@ import { NativeScriptCommonModule } from "nativescript-angular/common";
 
 import { SearchRoutingModule } from "./search-routing.module";
 import { SearchComponent } from "./search.component";
+import {NativeScriptFormsModule} from "nativescript-angular";
 
 @NgModule({
     imports: [
         NativeScriptCommonModule,
-        SearchRoutingModule
+        SearchRoutingModule,
+        NativeScriptFormsModule
     ],
     declarations: [
         SearchComponent
