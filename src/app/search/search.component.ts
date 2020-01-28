7a55e50478eaa04c40e3792edaf962ee167a425c
Alexander Hodes <alexander.hodes@live.com>
2020-01-28 19:31:04 +0100

    improved camera

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,6 +9,11 @@ import * as Camera from "nativescript-camera";
 import { Image } from "tns-core-modules/ui/image";
 import {WhatThreeWordsService} from "~/app/shared/services/what.three.words.service";
 import {WhatsThreeWords} from "~/app/shared/models/what.three.words.model";
+import {knownFolders, path, File} from "tns-core-modules/file-system";
+import {ImageAsset} from "tns-core-modules/image-asset";
+import {ImageSource} from "tns-core-modules/image-source";
+import {ImageService} from "~/app/shared/services/image.service";
+import {Picture} from "~/app/shared/models/picture.models";
 
 @Component({
     selector: "Search",
@@ -21,19 +26,24 @@ export class SearchComponent implements OnInit {
     latitude: number;
     timestamp: Date;
     words: string;
+    imageSrc: string;
+    count: number;
 
     constructor(private platformService: PlatformService,
                 private geoLocationService: GeolocationService,
                 private notificationService: NotificationService,
                 private fileService: FileService,
-                private whatThreeWordsService: WhatThreeWordsService) {
+                private whatThreeWordsService: WhatThreeWordsService,
+                private imageService: ImageService) {
         // Use the constructor to inject services.
         this.platformService.getProperties();
 //        this.notificationService.schedule();
+        this.count = 0;
     }
 
     ngOnInit(): void {
         // Use the "ngOnInit" handler to initialize data for the view.
+        this.imageSrc = "/data/user/0/org.nativescript.emanativescript/files/test.jpg";
     }
 
     trackPosition(): void {
@@ -48,13 +58,50 @@ export class SearchComponent implements OnInit {
     }
 
     takePhoto(): void {
-        Camera.takePicture().
-        then((imageAsset) => {
-            console.log("Result is an image asset instance");
-            let image = new Image();
-            image.src = imageAsset;
-        }).catch((err) => {
-            console.log("Error -> " + err.message);
+        const permission = Camera.requestPermissions().then(
+            function success() {
+                return true;
+            },
+            function error() {
+                console.log('kein Zugriff auf die Kamera');
+                return false;
+            }
+        );
+
+        const imageAssetPromise = permission.then((value: boolean) => {
+           if (value) {
+               const options = { saveToGallery: false };
+               return Camera.takePicture(options);
+           }
+        });
+
+        imageAssetPromise.then((imageAsset: ImageAsset) => this.saveImage(imageAsset));
+    }
+
+    saveImage(imageAsset: ImageAsset): void {
+        const source = new ImageSource();
+
+        source.fromAsset(imageAsset).then((imageSource: ImageSource) => {
+
+            const folderPath: string = knownFolders.documents().path;
+            const fileName: string = `test-${this.count}.jpg`;
+            const filePath: string = path.join(folderPath, fileName);
+            const saved: boolean = imageSource.saveToFile(filePath, "jpg");
+
+            if (saved) {
+                console.log("Saved: " + filePath);
+                console.log("Image saved successfully!");
+                this.imageSrc = filePath;
+                this.count += 1;
+
+                const picture: Picture = {
+                    url: filePath,
+                    liked: false,
+                    name: fileName
+                };
+
+                this.imageService.addImage(picture);
+            }
         });
     }
 
@@ -77,5 +124,11 @@ export class SearchComponent implements OnInit {
                 this.words = response.words;
             });
         }
+    }
+
+    reloadImage(): void {
+        const oldPath = this.imageSrc;
+        this.imageSrc = '';
+        this.imageSrc = oldPath;
     }
 }
