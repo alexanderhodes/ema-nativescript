f917dc0a4512b2c4259fcd8a5fdca6a6e4efd345
Alexander Hodes <alexander.hodes@live.com>
2020-01-31 16:38:14 +0100

    pushy error handling when no connection

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { DataService, IDataItem } from "../shared/services/data.service";
 import {getDevicePushToken, setNotificationHandler} from "nativescript-pushy";
+import {ConnectivityService} from "~/app/shared/services/connectivity.service";
+import * as Toast from "nativescript-toast";
 
 @Component({
     selector: "Home",
@@ -9,13 +11,19 @@ import {getDevicePushToken, setNotificationHandler} from "nativescript-pushy";
 export class HomeComponent implements OnInit {
     items: Array<IDataItem>;
 
-    constructor(private _itemService: DataService) {
-        getDevicePushToken()
-            .then(token => console.log(`getDevicePushToken success, token: ${token}`))
-            .catch(err => console.log(`getDevicePushToken error: ${err}`));
-        setNotificationHandler(notification => {
-            console.log(`Notification received: ${JSON.stringify(notification)}`);
-        });
+    constructor(private _itemService: DataService,
+                private connectivityService: ConnectivityService) {
+        const connectionType = this.connectivityService.checkConnectionType();
+        if (connectionType === "Wi-Fi" || connectionType === "Mobile") {
+            getDevicePushToken()
+                .then(token => console.log(`getDevicePushToken success, token: ${token}`))
+                .catch(err => console.log(`getDevicePushToken error: ${err}`));
+            setNotificationHandler(notification => {
+                console.log(`Notification received: ${JSON.stringify(notification)}`);
+            });
+        } else {
+            Toast.makeText("Es konnte keine Verbindung zu Pushy aufgebaut werden", "long").show();
+        }
     }
 
     ngOnInit(): void {
