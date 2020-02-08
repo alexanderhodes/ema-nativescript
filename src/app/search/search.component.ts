2d75e18eac14fe94a35866c94b7d212ccefdf880
Alexander Hodes <alexander.hodes@live.com>
2020-02-08 14:12:33 +0100

    finished search component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,24 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import {PlatformService} from "~/app/shared/services/platform.service";
+import {Component, OnInit} from "@angular/core";
 import {GeolocationService} from "~/app/shared/services/geolocation.service";
-import {TextField} from "tns-core-modules/ui/text-field";
-import {NotificationService} from "~/app/shared/services/notification.service";
-import {FileService} from "~/app/shared/services/file.service";
 import * as geolocation from "nativescript-geolocation";
-import * as Camera from "nativescript-camera";
 import {WhatThreeWordsService} from "~/app/shared/services/what.three.words.service";
 import {WhatsThreeWords} from "~/app/shared/models/what.three.words.model";
-import {knownFolders, path, File} from "tns-core-modules/file-system";
-import {ImageAsset} from "tns-core-modules/image-asset";
-import {ImageSource} from "tns-core-modules/image-source";
-import {ImageService} from "~/app/shared/services/image.service";
-import {Picture} from "~/app/shared/models/picture.models";
 import {BluetoothService} from "~/app/shared/services/bluetooth.service";
-import {DatabaseService} from "~/app/shared/services/database.service";
-import { registerElement } from "nativescript-angular/element-registry";
-registerElement("Mapbox", () => require("nativescript-mapbox").MapboxView);
-import * as mapbox from "nativescript-mapbox";
+import {registerElement} from "nativescript-angular/element-registry";
 import {ApiTokenService} from "~/app/shared/services/api-token.service";
+import {ConnectivityService} from "~/app/shared/services/connectivity.service";
+import * as Toast from "nativescript-toast";
+
+registerElement("Mapbox", () => require("nativescript-mapbox").MapboxView);
 
 @Component({
     selector: "Search",
@@ -26,38 +17,30 @@ import {ApiTokenService} from "~/app/shared/services/api-token.service";
 })
 export class SearchComponent implements OnInit {
 
-    name: string;
     longitude: number;
     latitude: number;
     timestamp: Date;
     words: string;
-    imageSrc: string;
-    count: number;
     located: boolean;
+    nearestPlace: string;
+    private _hasConnection: boolean;
 
-    constructor(private platformService: PlatformService,
-                private geoLocationService: GeolocationService,
-                private notificationService: NotificationService,
-                private fileService: FileService,
+    constructor(private geoLocationService: GeolocationService,
                 private whatThreeWordsService: WhatThreeWordsService,
-                private imageService: ImageService,
                 private bluetoothService: BluetoothService,
-                private databaseService: DatabaseService,
-                private apiTokenService: ApiTokenService) {
+                private apiTokenService: ApiTokenService,
+                private connectivityService: ConnectivityService) {
         // Use the constructor to inject services.
-        this.platformService.getProperties();
-//        this.notificationService.schedule();
-        this.count = 0;
         this.located = false;
+
+        this.connectivityService.hasInternetConnection().subscribe(hasConnection => this._hasConnection = hasConnection);
     }
 
     ngOnInit(): void {
         // Use the "ngOnInit" handler to initialize data for the view.
-        this.imageSrc = "/data/user/0/org.nativescript.emanativescript/files/test.jpg";
     }
 
     trackPosition(): void {
-        console.log('trackPosition');
         this.geoLocationService.getCurrentLocation()
             .pipe()
             .subscribe((res: geolocation.Location) => {
@@ -65,12 +48,12 @@ export class SearchComponent implements OnInit {
                 this.longitude = res.longitude;
                 this.timestamp = res.timestamp;
                 this.located = true;
-            });
+            }, error => Toast.makeText('Die Position konnte nicht ermittelt werden', 'short'));
     }
 
     onMapReady(args) {
         // you can tap into the native MapView objects (MGLMapView for iOS and com.mapbox.mapboxsdk.maps.MapView for Android)
-        var nativeMapView = args.ios ? args.ios : args.android;
+        const nativeMapView = args.ios ? args.ios : args.android;
         console.log("Mapbox onMapReady for " + (args.ios ? "iOS" : "Android") + ", native object received: " + nativeMapView);
 
         // .. or use the convenience methods exposed on args.map, for instance:
@@ -80,79 +63,24 @@ export class SearchComponent implements OnInit {
                 lng: this.longitude,
                 title: 'Aktuelle Position',
                 subtitle: 'Deine Position',
-                selected: true, // makes the callout show immediately when the marker is added (note: only 1 marker can be selected at a time)
+                selected: true,
                 onCalloutTap: function(){console.log("'Nice location' marker callout tapped");}
-            }]
-        );
-    }
-
-    takePhoto(): void {
-        const permission = Camera.requestPermissions().then(
-            function success() {
-                return true;
-            },
-            function error() {
-                console.log('kein Zugriff auf die Kamera');
-                return false;
-            }
-        );
-
-        const imageAssetPromise = permission.then((value: boolean) => {
-           if (value) {
-               const options = { saveToGallery: false };
-               return Camera.takePicture(options);
-           }
-        });
-
-        imageAssetPromise.then((imageAsset: ImageAsset) => this.saveImage(imageAsset));
-    }
-
-    saveImage(imageAsset: ImageAsset): void {
-        const source = new ImageSource();
-
-        source.fromAsset(imageAsset).then((imageSource: ImageSource) => {
-
-            const folderPath: string = knownFolders.documents().path;
-            const fileName: string = `test-${this.count}.jpg`;
-            const filePath: string = path.join(folderPath, fileName);
-            const saved: boolean = imageSource.saveToFile(filePath, "jpg");
-
-            if (saved) {
-                console.log("Saved: " + filePath);
-                console.log("Image saved successfully!");
-                this.imageSrc = filePath;
-                this.count += 1;
-
-                const picture: Picture = {
-                    url: filePath,
-                    liked: false,
-                    name: fileName
-                };
-
-//                this.imageService.addImage(picture);
-                this.databaseService.insert(picture);
-            }
-        });
-    }
-
-    onReturnPress(event: any): void {
-        let textField = <TextField> event.object;
-
-        console.log('name', textField.text);
-        console.log('ngModel', textField.text);
-
-        const fileName = 'test.txt';
-        const content = 'Das ist ein Test, um in eine Datei zu schreiben.';
-        this.fileService.createFile(fileName, content);
-        this.fileService.readFile(fileName);
+            }]);
     }
 
     findWordsByPosition() {
-        if (this.longitude && this.latitude) {
-            this.whatThreeWordsService.get(this.longitude, this.latitude).subscribe((response: WhatsThreeWords) => {
-                console.log('words', response.words);
-                this.words = response.words;
-            });
+        if (this.hasConnection) {
+            if (this.longitude && this.latitude) {
+                this.whatThreeWordsService.get(this.longitude, this.latitude).subscribe((response: WhatsThreeWords) => {
+                    console.log('words', response.words);
+                    this.words = response.words;
+                    this.nearestPlace = response.nearestPlace;
+                });
+            } else {
+                Toast.makeText('Position muss zunächst ermittelt werden', 'short').show();
+            }
+        } else {
+            Toast.makeText('Keine Internet-Verbindung', 'short').show();
         }
     }
 
@@ -160,13 +88,11 @@ export class SearchComponent implements OnInit {
         return this.apiTokenService.mapbox;
     }
 
-    reloadImage(): void {
-        const oldPath = this.imageSrc;
-        this.imageSrc = '';
-        this.imageSrc = oldPath;
+    scan(): void {
+        // this.bluetoothService.startScanning();
     }
 
-    scan(): void {
-        this.bluetoothService.startScanning();
+    get hasConnection(): boolean {
+        return this._hasConnection;
     }
 }
--- src/app/shared/services/bluetooth.service.ts
+++ src/app/shared/services/bluetooth.service.ts
@@ -1,23 +1,82 @@
 import {Injectable} from "@angular/core";
-import * as Bluetooth from "nativescript-bluetooth";
-
+// import {Bluetooth, StartScanningOptions} from "nativescript-bluetooth";
 
 @Injectable({
     providedIn: "root"
 })
 export class BluetoothService {
 
-    constructor() {}
-
-    startScanning() {
-        Bluetooth.startScanning({
-            serviceUUIDs: [],
-            seconds: 4,
-            onDiscovered: function (peripheral) {
-                console.log("Periperhal found with UUID: " + peripheral.UUID);
-            },
-            skipPermissionCheck: false
-        }).then();
+    constructor() {
     }
+
+    // startScanning(): void {
+    //     this.isBluetoothEnabled().then(enabled => {
+    //         if (enabled) {
+    //             const options: StartScanningOptions = {
+    //                 seconds: 4,
+    //                 onDiscovered: function (peripheral) {
+    //                     console.log("Periperhal found with UUID: " + peripheral.UUID);
+    //                 },
+    //                 skipPermissionCheck: false
+    //             };
+    //             Bluetooth.getBluetoothInstance().startScanning(options).then(result => {
+    //                 console.log('result', result);
+    //             });
+    //         }
+    //     })
+    // }
+    //
+    // private isBluetoothEnabled() {
+    //     return Bluetooth.getBluetoothInstance().isBluetoothEnabled().then(enabled => {
+    //         console.log('enabled', enabled);
+    //         return enabled;
+    //     }, error => {
+    //         return false;
+    //     });
+    // }
+
+    // startScanning() {
+    //     this.checkBluetoothConnection().then(enabled => {
+    //         if (enabled) {
+    //             Bluetooth.hasCoarseLocationPermission().then(granted => {
+    //                 console.log('granted', granted);
+    //                 if (granted) {
+    //                     Bluetooth.startScanning({
+    //                         serviceUUIDs: [],
+    //                         seconds: 4,
+    //                         onDiscovered: function (peripheral) {
+    //                             console.log("Periperhal found with UUID: " + peripheral.UUID);
+    //                         },
+    //                         skipPermissionCheck: false
+    //                     }).then(result => {
+    //                         console.log('result', result);
+    //                     });
+    //                 }
+    //             });
+    //         } else {
+    //             console.log('bluetooth not enabled');
+    //         }
+    //     }, error => {
+    //         console.log('bluetooth not enabled');
+    //     })
+    //
+    // }
+    //
+    // private checkBluetoothConnection() {
+    //     return Bluetooth.isBluetoothEnabled().then((enabled: boolean) => {
+    //         if (enabled) {
+    //             return true;
+    //         } else {
+    //             Bluetooth.enable().then(result => {
+    //                 return result;
+    //             }, error => {
+    //                 return false;
+    //             })
+    //         }
+    //     }, error => {
+    //         return false;
+    //     });
+    // }
+
 
 }
